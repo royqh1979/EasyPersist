2242a1d94b9f2befa33a73566108234775f7d5b8
Roy <quhua@bjfu.edu.cn>
2017-02-21 15:48:47 +0800

    * fix: can't find xsd files

--- src/net/royqh/easypersist/EasyPersistResourceProvider.java
+++ src/net/royqh/easypersist/EasyPersistResourceProvider.java
@@ -10,10 +10,8 @@ import com.intellij.openapi.application.ApplicationManager;
 public class EasyPersistResourceProvider implements StandardResourceProvider {
     @Override
     public void registerResources(ResourceRegistrar registrar) {
-        if(ApplicationManager.getApplication().isUnitTestMode()) {
-            registrar.addStdResource("http://royqh.net/codegenerator/orm/schema/config_1_0.xsd", "/resources/schemas/config_1_0.xsd", this.getClass());
-            registrar.addStdResource("http://royqh.net/codegenerator/orm/schema/config_1_1.xsd", "/resources/schemas/config_1_1.xsd", this.getClass());
-            registrar.addStdResource("http://royqh.net/codegenerator/orm/schema/config_1_2.xsd", "/resources/schemas/config_1_2.xsd", this.getClass());
-        }
+        registrar.addStdResource("http://royqh.net/codegenerator/orm/schema/config_1_0.xsd", "/schemas/config_1_0.xsd", this.getClass());
+        registrar.addStdResource("http://royqh.net/codegenerator/orm/schema/config_1_1.xsd", "/schemas/config_1_1.xsd", this.getClass());
+        registrar.addStdResource("http://royqh.net/codegenerator/orm/schema/config_1_2.xsd", "/schemas/config_1_2.xsd", this.getClass());
     }
 }
