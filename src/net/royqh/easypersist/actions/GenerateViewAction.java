f7955ca64be58eda0d29401a7c3d0b33d9a5e06d
Roy <royqh1979@gmail.com>
2017-12-20 09:53:29 +0800

    * fix: display success notification after press CANCEL in View Generation Config Dialog

--- src/net/royqh/easypersist/actions/GenerateViewAction.java
+++ src/net/royqh/easypersist/actions/GenerateViewAction.java
@@ -7,6 +7,7 @@ import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.command.WriteCommandAction;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.module.ModuleUtil;
@@ -14,13 +15,27 @@ import com.intellij.openapi.progress.ProgressIndicator;
 import com.intellij.openapi.progress.ProgressManager;
 import com.intellij.openapi.progress.Task;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.roots.ProjectRootManager;
 import com.intellij.openapi.util.Computable;
-import com.intellij.psi.PsiClass;
-import com.intellij.psi.PsiElement;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.*;
+import com.intellij.psi.codeStyle.CodeStyleManager;
+import net.royqh.easypersist.entity.generator.persistor.MySQLGenerator;
+import net.royqh.easypersist.entity.generator.persistor.PersistorMethodGenerator;
+import net.royqh.easypersist.entity.generator.persistor.PersistorsGenerator;
+import net.royqh.easypersist.entity.generator.service.FactTableServiceGenerator;
+import net.royqh.easypersist.entity.generator.service.ServiceGenerator;
+import net.royqh.easypersist.entity.generator.view.ControllerGenerator;
+import net.royqh.easypersist.entity.generator.view.FactTableControllerGenerator;
+import net.royqh.easypersist.entity.generator.view.FactTableViewGenerator;
+import net.royqh.easypersist.entity.generator.view.ViewGenerator;
 import net.royqh.easypersist.entity.model.Entity;
 import net.royqh.easypersist.entity.parser.jpa.ClassParser;
 import net.royqh.easypersist.ui.EditorTypeChooseDialog;
+import net.royqh.easypersist.ui.ViewType;
 import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
 
 /**
  * Created by Roy on 2017/6/12.
@@ -32,10 +47,8 @@ public class GenerateViewAction extends AnAction {
     public void actionPerformed(AnActionEvent e) {
         ProgressManager progressManager = ProgressManager.getInstance();
         progressManager.run(new Task.Backgroundable(e.getProject(), "Parsing Entity...") {
-
             @Override
             public void run(@NotNull ProgressIndicator indicator) {
-
                 try {
                     PsiClass psiClass = (PsiClass) e.getData(CommonDataKeys.PSI_ELEMENT);
                     Project project = e.getProject();
@@ -43,17 +56,65 @@ public class GenerateViewAction extends AnAction {
                     Entity entity = ApplicationManager.getApplication().runReadAction(new Computable<Entity>() {
                         @Override
                         public Entity compute() {
-                            indicator.setFraction(0.5);
+                            indicator.setFraction(0.1);
                             indicator.setText("Parsing Entity " + psiClass.getQualifiedName());
                             return ClassParser.parseEntityClassWithReferences(psiClass, module, true);
                         }
                     });
-                    indicator.setFraction(1);
-                    EditorTypeChooseDialog dialog = new EditorTypeChooseDialog(entity,psiClass,module);
+                    EditorTypeChooseDialog dialog = new EditorTypeChooseDialog(entity);
                     dialog.updateUI();
                     dialog.pack();
                     dialog.setLocationRelativeTo(null);
                     dialog.setVisible(true);
+
+                    if (!dialog.isGoOn()) {
+                        return;
+                    }
+
+                    ViewType viewType = dialog.getViewType();
+                    boolean importSelected = dialog.isImportSelected();
+                    boolean exportSelected = dialog.isExportSelected();
+
+                    VirtualFile root = ProjectRootManager.getInstance(project)
+                            .getFileIndex().getContentRootForFile(psiClass.getContainingFile().getVirtualFile());
+                    WriteCommandAction.runWriteCommandAction(project, new Runnable() {
+                        @Override
+                        public void run() {
+                            try {
+                                VirtualFile genDir = root.findChild("gen");
+                                if (genDir == null) {
+                                    genDir = root.createChildDirectory(project, "gen");
+                                }
+                                if (genDir == null) {
+                                    throw new RuntimeException("Can't create folder gen!");
+                                }
+                                PsiDirectory psiOutputDir = PsiManager.getInstance(module.getProject()).findDirectory(genDir);
+
+                                PsiFileFactory psiFileFactory = PsiFileFactory.getInstance(module.getProject());
+                                JavaPsiFacade facade = JavaPsiFacade.getInstance(module.getProject());
+                                CodeStyleManager codeStyleManager = CodeStyleManager.getInstance(module.getProject());
+                                PersistorMethodGenerator methodGenerator = new PersistorMethodGenerator(new MySQLGenerator());
+                                PersistorsGenerator persistorsGenerator = new PersistorsGenerator(methodGenerator);
+
+                                if (viewType == ViewType.FactTableEditorOnly) {
+                                    generateFactTableView(entity,module,psiOutputDir, psiFileFactory, facade, codeStyleManager, persistorsGenerator, indicator);
+                                } else {
+
+                                    generateViewFiles(entity,module,psiOutputDir, psiFileFactory, facade, codeStyleManager, persistorsGenerator, indicator, viewType, importSelected, exportSelected);
+                                }
+
+                                Notification notification = new Notification(
+                                        "Easy Persist",
+                                        "Success",
+                                        "Entity " + psiClass.getName() + " 's editor code generation finished.",
+                                        NotificationType.INFORMATION
+                                );
+                                Notifications.Bus.notify(notification, module.getProject());
+                            } catch (IOException e1) {
+                                throw new RuntimeException(e1);
+                            }
+                        }
+                    });
                 } catch (Exception exception) {
                     exception.printStackTrace();
                     Notification notification = new Notification(
@@ -65,15 +126,65 @@ public class GenerateViewAction extends AnAction {
                     Notifications.Bus.notify(notification, e.getProject());
                     logger.error(exception);
                 }
-
             }
         });
-
     }
 
     @Override
     public void update(AnActionEvent e) {
         PsiElement element = e.getData(CommonDataKeys.PSI_ELEMENT);
         e.getPresentation().setVisible(ClassParser.isEntityClass(element));
+    }
+
+    private void generateViewFiles(Entity entity, Module module, PsiDirectory psiOutputDir, PsiFileFactory psiFileFactory, JavaPsiFacade facade, CodeStyleManager codeStyleManager, PersistorsGenerator persistorsGenerator, ProgressIndicator indicator, ViewType viewType, boolean importEnabled, boolean exportEnabled) {
+        indicator.setText(String.format("Generating persistors for %s ", entity.getClassInfo().getQualifiedName()));
+        indicator.setFraction(0.2);
+        persistorsGenerator.generatePersistor(psiFileFactory, facade, codeStyleManager, entity, psiOutputDir);
+        persistorsGenerator.generatePersistorCompositor(psiFileFactory, facade, codeStyleManager, entity, psiOutputDir);
+        indicator.setText(String.format("Generating service for %s ", entity.getClassInfo().getQualifiedName()));
+        indicator.setFraction(0.4);
+        ServiceGenerator.generateService(viewType, psiFileFactory, codeStyleManager, entity, psiOutputDir, module, importEnabled, exportEnabled);
+        indicator.setText(String.format("Generating controller for %s ", entity.getClassInfo().getQualifiedName()));
+        indicator.setFraction(0.2);
+        ControllerGenerator.generateController(viewType, psiFileFactory, codeStyleManager, entity, psiOutputDir, module, importEnabled, exportEnabled);
+        indicator.setText(String.format("Generating view jsp files for %s ", entity.getClassInfo().getQualifiedName()));
+        indicator.setFraction(0.8);
+        ViewGenerator.generateJspViews(viewType, entity, psiOutputDir, importEnabled, exportEnabled);
+        indicator.setFraction(1);
+    }
+
+    private void generateFactTableView(Entity entity, Module module, PsiDirectory psiOutputDir, PsiFileFactory psiFileFactory, JavaPsiFacade facade, CodeStyleManager codeStyleManager, PersistorsGenerator persistorsGenerator, @NotNull ProgressIndicator indicator) {
+        Entity factTableEntity = entity;
+        Entity baseEntity = factTableEntity.getMappingRepository().findEntityByClass(factTableEntity.getFactTableInfo().getEntityKeyProperty().getRefEntityFullClassName());
+        Entity propertyEntity = factTableEntity.getMappingRepository().findEntityByClass(factTableEntity.getFactTableInfo().getPropertyKeyProperty().getRefEntityFullClassName());
+
+        indicator.setText(String.format("Generating persistors for %s ", factTableEntity.getClassInfo().getQualifiedName()));
+        indicator.setFraction(0.2);
+        persistorsGenerator.generatePersistor(psiFileFactory, facade, codeStyleManager, factTableEntity, psiOutputDir);
+        persistorsGenerator.generatePersistorCompositor(psiFileFactory, facade, codeStyleManager, factTableEntity, psiOutputDir);
+        indicator.setText(String.format("Generating persistors for %s ", baseEntity.getClassInfo().getQualifiedName()));
+        indicator.setFraction(0.3);
+        persistorsGenerator.generatePersistor(psiFileFactory, facade, codeStyleManager, baseEntity, psiOutputDir);
+        persistorsGenerator.generatePersistorCompositor(psiFileFactory, facade, codeStyleManager, baseEntity, psiOutputDir);
+        indicator.setText(String.format("Generating persistors for %s ", propertyEntity.getClassInfo().getQualifiedName()));
+        indicator.setFraction(0.4);
+        persistorsGenerator.generatePersistor(psiFileFactory, facade, codeStyleManager, propertyEntity, psiOutputDir);
+        persistorsGenerator.generatePersistorCompositor(psiFileFactory, facade, codeStyleManager, propertyEntity, psiOutputDir);
+        indicator.setText(String.format("Generating service for %s ", factTableEntity.getClassInfo().getQualifiedName()));
+        indicator.setFraction(0.5);
+        FactTableServiceGenerator.generateService(psiFileFactory, codeStyleManager, factTableEntity, psiOutputDir, module);
+        indicator.setText(String.format("Generating service for %s ", propertyEntity.getClassInfo().getQualifiedName()));
+        indicator.setFraction(0.6);
+        ServiceGenerator.generateService(ViewType.ExcelStyleEditorOnly, psiFileFactory, codeStyleManager, propertyEntity, psiOutputDir, module, false, false);
+        indicator.setText(String.format("Generating service for %s ", baseEntity.getClassInfo().getQualifiedName()));
+        indicator.setFraction(0.7);
+        ServiceGenerator.generateService(ViewType.FactTableEditorOnly, psiFileFactory, codeStyleManager, baseEntity, psiOutputDir, module, false, false);
+        indicator.setText(String.format("Generating controller for %s ", factTableEntity.getClassInfo().getQualifiedName()));
+        indicator.setFraction(0.8);
+        FactTableControllerGenerator.generateController(psiFileFactory, codeStyleManager, factTableEntity, psiOutputDir, module);
+        indicator.setText(String.format("Generating view jsp files for %s ", factTableEntity.getClassInfo().getQualifiedName()));
+        indicator.setFraction(0.9);
+        FactTableViewGenerator.generateView(factTableEntity, psiOutputDir);
+        indicator.setFraction(1);
     }
 }
--- src/net/royqh/easypersist/ui/EditorTypeChooseDialog.java
+++ src/net/royqh/easypersist/ui/EditorTypeChooseDialog.java
@@ -39,7 +39,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class EditorTypeChooseDialog extends JDialog {
-    private PsiClass psiClass;
     private JPanel contentPane;
     private JButton buttonOK;
     private JButton buttonCancel;
@@ -54,13 +53,14 @@ public class EditorTypeChooseDialog extends JDialog {
     private ImageView imgEditorDemo;
     private Entity entity;
     private Logger logger = Logger.getLogger(EditorTypeChooseDialog.class);
-    private Module module;
     private Map<String, BufferedImage> preLoadedImages = new HashMap<>();
+    private boolean goOn;
+    private ViewType viewType;
+    private boolean importSelected;
+    private boolean exportSelected;
 
-    public EditorTypeChooseDialog(Entity entity, PsiClass psiClass, Module module) {
+    public EditorTypeChooseDialog(Entity entity) {
         this.entity = entity;
-        this.psiClass = psiClass;
-        this.module = module;
         $$$setupUI$$$();
         setContentPane(contentPane);
         setModal(true);
@@ -165,127 +165,32 @@ public class EditorTypeChooseDialog extends JDialog {
     }
 
     private void onCancel() {
-        // add your code here if necessary
+        goOn = false;
         dispose();
     }
 
     private void onOK() {
-        ProgressManager progressManager = ProgressManager.getInstance();
-        progressManager.run(new Task.Backgroundable(module.getProject(), "Generate Editor Persistor") {
-            @Override
-            public void run(@NotNull ProgressIndicator indicator) {
-                try {
-                    indicator.setFraction(0.1);
-                    ViewType viewType = (ViewType) cbViewType.getSelectedItem();
-                    VirtualFile root = ProjectRootManager.getInstance(module.getProject())
-                            .getFileIndex().getContentRootForFile(psiClass.getContainingFile().getVirtualFile());
-                    WriteCommandAction.runWriteCommandAction(module.getProject(), new Runnable() {
-
-                        @Override
-                        public void run() {
-                            try {
-                                VirtualFile genDir = root.findChild("gen");
-                                if (genDir == null) {
-                                    genDir = root.createChildDirectory(module.getProject(), "gen");
-                                }
-                                if (genDir == null) {
-                                    throw new RuntimeException("Can't create folder gen!");
-                                }
-                                PsiDirectory psiOutputDir = PsiManager.getInstance(module.getProject()).findDirectory(genDir);
-
-                                PsiFileFactory psiFileFactory = PsiFileFactory.getInstance(module.getProject());
-                                JavaPsiFacade facade = JavaPsiFacade.getInstance(module.getProject());
-                                CodeStyleManager codeStyleManager = CodeStyleManager.getInstance(module.getProject());
-                                PersistorMethodGenerator methodGenerator = new PersistorMethodGenerator(new MySQLGenerator());
-                                PersistorsGenerator persistorsGenerator = new PersistorsGenerator(methodGenerator);
-
-                                if (viewType == ViewType.FactTableEditorOnly) {
-                                    generateFactTableView(psiOutputDir, psiFileFactory, facade, codeStyleManager, persistorsGenerator, indicator);
-                                } else {
-                                    generateViewFiles(psiOutputDir, psiFileFactory, facade, codeStyleManager, persistorsGenerator, indicator, viewType, cbImport.isSelected(), cbExport.isSelected());
-                                }
-
-                                Notification notification = new Notification(
-                                        "Easy Persist",
-                                        "Success",
-                                        "Entity " + psiClass.getName() + " 's editor code generation finished.",
-                                        NotificationType.INFORMATION
-                                );
-                                Notifications.Bus.notify(notification, module.getProject());
-                            } catch (IOException e1) {
-                                throw new RuntimeException(e1);
-                            }
-                        }
-                    });
-                } catch (Exception exception) {
-                    exception.printStackTrace();
-                    Notification notification = new Notification(
-                            "Easy Persist",
-                            "Error",
-                            "Generation failed :" + exception.getMessage(),
-                            NotificationType.ERROR
-                    );
-                    Notifications.Bus.notify(notification, module.getProject());
-                    logger.error(exception);
-                } finally {
-                    indicator.setFraction(1);
-                }
-            }
-        });
-
+        goOn = true;
+        viewType = (ViewType) cbViewType.getSelectedItem();
+        importSelected = cbImport.isSelected();
+        exportSelected = cbExport.isSelected();
         dispose();
     }
 
-    private void generateViewFiles(PsiDirectory psiOutputDir, PsiFileFactory psiFileFactory, JavaPsiFacade facade, CodeStyleManager codeStyleManager, PersistorsGenerator persistorsGenerator, ProgressIndicator indicator, ViewType viewType, boolean importEnabled, boolean exportEnabled) {
-        indicator.setText(String.format("Generating persistors for %s ", entity.getClassInfo().getQualifiedName()));
-        indicator.setFraction(0.2);
-        persistorsGenerator.generatePersistor(psiFileFactory, facade, codeStyleManager, entity, psiOutputDir);
-        persistorsGenerator.generatePersistorCompositor(psiFileFactory, facade, codeStyleManager, entity, psiOutputDir);
-        indicator.setText(String.format("Generating service for %s ", entity.getClassInfo().getQualifiedName()));
-        indicator.setFraction(0.4);
-        ServiceGenerator.generateService(viewType, psiFileFactory, codeStyleManager, entity, psiOutputDir, module, importEnabled, exportEnabled);
-        indicator.setText(String.format("Generating controller for %s ", entity.getClassInfo().getQualifiedName()));
-        indicator.setFraction(0.2);
-        ControllerGenerator.generateController(viewType, psiFileFactory, codeStyleManager, entity, psiOutputDir, module, importEnabled, exportEnabled);
-        indicator.setText(String.format("Generating view jsp files for %s ", entity.getClassInfo().getQualifiedName()));
-        indicator.setFraction(0.8);
-        ViewGenerator.generateJspViews(viewType, entity, psiOutputDir, importEnabled, exportEnabled);
-        indicator.setFraction(1);
+    public boolean isGoOn() {
+        return goOn;
     }
 
-    private void generateFactTableView(PsiDirectory psiOutputDir, PsiFileFactory psiFileFactory, JavaPsiFacade facade, CodeStyleManager codeStyleManager, PersistorsGenerator persistorsGenerator, @NotNull ProgressIndicator indicator) {
-        Entity factTableEntity = entity;
-        Entity baseEntity = factTableEntity.getMappingRepository().findEntityByClass(factTableEntity.getFactTableInfo().getEntityKeyProperty().getRefEntityFullClassName());
-        Entity propertyEntity = factTableEntity.getMappingRepository().findEntityByClass(factTableEntity.getFactTableInfo().getPropertyKeyProperty().getRefEntityFullClassName());
+    public ViewType getViewType() {
+        return viewType;
+    }
 
-        indicator.setText(String.format("Generating persistors for %s ", factTableEntity.getClassInfo().getQualifiedName()));
-        indicator.setFraction(0.2);
-        persistorsGenerator.generatePersistor(psiFileFactory, facade, codeStyleManager, factTableEntity, psiOutputDir);
-        persistorsGenerator.generatePersistorCompositor(psiFileFactory, facade, codeStyleManager, factTableEntity, psiOutputDir);
-        indicator.setText(String.format("Generating persistors for %s ", baseEntity.getClassInfo().getQualifiedName()));
-        indicator.setFraction(0.3);
-        persistorsGenerator.generatePersistor(psiFileFactory, facade, codeStyleManager, baseEntity, psiOutputDir);
-        persistorsGenerator.generatePersistorCompositor(psiFileFactory, facade, codeStyleManager, baseEntity, psiOutputDir);
-        indicator.setText(String.format("Generating persistors for %s ", propertyEntity.getClassInfo().getQualifiedName()));
-        indicator.setFraction(0.4);
-        persistorsGenerator.generatePersistor(psiFileFactory, facade, codeStyleManager, propertyEntity, psiOutputDir);
-        persistorsGenerator.generatePersistorCompositor(psiFileFactory, facade, codeStyleManager, propertyEntity, psiOutputDir);
-        indicator.setText(String.format("Generating service for %s ", factTableEntity.getClassInfo().getQualifiedName()));
-        indicator.setFraction(0.5);
-        FactTableServiceGenerator.generateService(psiFileFactory, codeStyleManager, factTableEntity, psiOutputDir, module);
-        indicator.setText(String.format("Generating service for %s ", propertyEntity.getClassInfo().getQualifiedName()));
-        indicator.setFraction(0.6);
-        ServiceGenerator.generateService(ViewType.ExcelStyleEditorOnly, psiFileFactory, codeStyleManager, propertyEntity, psiOutputDir, module, false, false);
-        indicator.setText(String.format("Generating service for %s ", baseEntity.getClassInfo().getQualifiedName()));
-        indicator.setFraction(0.7);
-        ServiceGenerator.generateService(ViewType.FactTableEditorOnly, psiFileFactory, codeStyleManager, baseEntity, psiOutputDir, module, false, false);
-        indicator.setText(String.format("Generating controller for %s ", factTableEntity.getClassInfo().getQualifiedName()));
-        indicator.setFraction(0.8);
-        FactTableControllerGenerator.generateController(psiFileFactory, codeStyleManager, factTableEntity, psiOutputDir, module);
-        indicator.setText(String.format("Generating view jsp files for %s ", factTableEntity.getClassInfo().getQualifiedName()));
-        indicator.setFraction(0.9);
-        FactTableViewGenerator.generateView(factTableEntity, psiOutputDir);
-        indicator.setFraction(1);
+    public boolean isImportSelected() {
+        return importSelected;
+    }
+
+    public boolean isExportSelected() {
+        return exportSelected;
     }
 
     /**
